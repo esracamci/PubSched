3d42ec99897ed405768d2afb857932c84c02a706
esracamci <46831824+esracamci@users.noreply.github.com>
2021-03-28 21:09:35 -0400

    Add files via upload

---
+++ pubmed7.py
@@ -0,0 +1,27 @@
+
+
+##### iterate through the list of names in terms.txt
+terms = open("names.txt").readlines()
+for g in terms:
+
+
+##### query pubmed for each term (author name), return PMIDs, print record
+    from Bio import Entrez
+    Entrez.email = "your-email@address.com" # per NCBI, please edit to contain your email 
+    handle = Entrez.esearch(db="pubmed", term=g, reldate=7) # reldate=n, where n is days from today
+    record = Entrez.read(handle)
+    handle.close()
+    idlist = record["IdList"]
+    #print(record["IdList"])
+    print(g)
+
+    from Bio import Medline
+    handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
+    records = Medline.parse(handle)
+    records = list(records)
+
+    for record in records:
+         print("  ", record.get("TI", "?"))
+         print("    ", record.get("AU", "?"))
+         print("    ", record.get("SO", "?"))
+         print("")
